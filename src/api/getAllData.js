7bc66ecd376324f55010031f51adbfe5771fb502
J Wood <73401121+DFK333@users.noreply.github.com>
2024-05-20 21:14:56 -0500

    got playerData from db to projections builder

---
+++ src/api/getAllData.js
@@ -0,0 +1,26 @@
+"use server";
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const client = new MongoClient(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    try {
+      await client.connect();
+      const database = client.db("projectionsBuilder"); // Choose a name for your database
+      const collection = database.collection("allPlayerData"); // Choose a name for your collection
+      const allData = await collection.find({}).toArray();
+
+      res.status(200).json(allData);
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong!" });
+    } finally {
+      await client.close();
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed!" });
+  }
+}
