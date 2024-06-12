3524835cbe013ec41553839c0abd519ca6491b1d
J Wood <73401121+DFK333@users.noreply.github.com>
2024-06-11 21:43:48 -0500

    major movement towards rewrite

--- src/app/toolkit/projectionBuilder/page.js
+++ src/app/toolkit/projectionBuilder/page.js
@@ -50,12 +50,58 @@ export default async function ProjectionsBuilder() {
   dataTest = JSON.parse(JSON.stringify(dataTest));
   // }, []);
 
+  //
+
+  async function fetchSleeperDataFromMongodb() {
+    const url =
+      "mongodb+srv://devJay:Hesstrucksarethebest@dailydynasties.syom4sb.mongodb.net/test";
+    const client = new MongoClient(url);
+
+    // The database to use
+    const dbName = "dailydynasties";
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+
+      // Use the collection "fantasycalcData"
+      const col = db.collection("sleeperNamesTeamsAndPositions");
+
+      // Construct a document
+
+      // Insert a single document, wait for promise so we can read it back
+      // const p = await col.insertOne(allFantasyCalcData);
+      // Find one document
+      const mySleperDoc = await col.findOne();
+
+      // console.log(mySleperDoc);
+      // playerData = mySleperDoc;
+
+      return mySleperDoc;
+      // Print to the console
+
+      ////////////////////////////////////
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
+    }
+  }
+
+  let sleeperData = await fetchSleeperDataFromMongodb();
+  // console.log(dataTest);
+  // React.useEffect(() => {
+  sleeperData = JSON.parse(JSON.stringify(sleeperData));
+
+ 
+ 
+
   return (
     <main className={styles.main}>
       <div className={styles.mainSiteTitleWrapper}>
         <div className={styles.pageTitle}>UN Projections Builder</div>
 
-        <ConstructProjections dataTest={dataTest} />
+        <ConstructProjections dataTest={dataTest} sleeperData={sleeperData} />
       </div>
     </main>
   );
