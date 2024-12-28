673bdf29f98f8d9a50467b2e78c47ea91a77f55e
Jay Wood <jwoodcodes@gmail.com>
2024-12-27 21:28:43 -0600

    startin UN site

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { Main } from "next/document";
+import MainNav from "./components/mainNav/mainNav"
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <MainNav />
       <div className={styles.mainSiteTitleWrapper}>
         <div className={styles.mainSiteTitle}>The Undroppables</div>
       </div>
-      <div className={styles.linkWrapper}>
-        <Link href={"/toolkit"} className={styles.mainNavLink}>
-          UN Toolkit
-        </Link>
-      </div>
+      
     </main>
   );
 }
