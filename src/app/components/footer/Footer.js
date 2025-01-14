40bb14ca2d6e7eaf8fd54b5b4c17b89d12a912cf
Jay Wood <jwoodcodes@gmail.com>
2025-01-13 20:48:08 -0600

    update home

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.Wrapper}>
+            
+            <p>Copyright &copy; 2025 The Undroppables</p>
+        </footer>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,25 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import MainNav from "./components/mainNav/MainNav"
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <MainNav />
       <div className={styles.mainSiteTitleWrapper}>
-        <div className={styles.mainSiteTitle}>The Undroppables</div>
+        {/* <div className={styles.mainSiteTitle}>The Undroppables</div> */}
+        <Image
+          src="/Art-of-Dynasty-Chapter-1-1.webp"
+          alt="The Undroppables Logo"
+          width={1000}
+          height={500}
+          className={styles.artOfDynastyImage}
+        />
         
       </div>
       
+      <Footer />
     </main>
   );
 }
