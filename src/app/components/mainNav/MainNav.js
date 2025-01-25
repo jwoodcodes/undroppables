30a4e2ecbb84b237cfaff6fbf0047aa08a757d4d
Jay Wood <jwoodcodes@gmail.com>
2025-01-24 21:00:46 -0600

    adding images

--- src/app/components/mainNav/MainNav.js
+++ src/app/components/mainNav/MainNav.js
@@ -5,23 +5,25 @@ import styles from "./mainNav.module.css";
 export default function MainNav() {
     return (
         <nav className={styles.mainNavWrapper}>
-            <div>
-                <a href="/" className={styles.mainLogo}>
-                <Image src="/TU-LOGO-WHITE-ON-TRANSPARENT.webp" alt="logo" width={250} height={40} />
-            </a>
-            </div>
+            
             <ul className={styles.navLinksWrapper}>
                 
-                <li ><a href="#" className={styles.navLink}>Tools</a></li>
+                <li ><a href="#" className={styles.navLink}>Podcasts</a></li>
                 
                 <li ><a href="#" className={styles.navLink}>Rankings</a></li>
-                <li ><a href="#" className={styles.navLink}>Youtube Shows</a></li>
+                
                 <li ><a href="#" className={styles.navLink}>The Undrafted</a></li>
-                <li ><a href="#" className={styles.navLink}>Underdog ADP</a></li>
-                <li ><a href="#" className={styles.navLink}>partnerships</a></li>
+                <li ><a href="#" className={styles.navLink}>ADP</a></li>
+                <li ><a href="#" className={styles.navLink}>Tools</a></li>
+                <li ><a href="#" className={styles.navLink}>Partnerships</a></li>
+                <li ><a href="#" className={styles.navLink}>Patreon</a></li>
 
                 
             </ul>
+
+            <div className={styles.socialsWrapper}>
+                Social Media: 
+            </div>
         </nav>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+
+import Header from "./components/header/Header";
 import MainNav from "./components/mainNav/MainNav"
 import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <MainNav />
       <div className={styles.mainSiteTitleWrapper}>
         {/* <div className={styles.mainSiteTitle}>The Undroppables</div> */}
-        <Image
-          src="/Art-of-Dynasty-Chapter-1-1.webp"
-          alt="The Undroppables Logo"
-          width={1000}
-          height={500}
-          className={styles.artOfDynastyImage}
-        />
+       
         
       </div>
       
