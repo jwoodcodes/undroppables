7563db59be274f525cec8d8f1fc4b4ca449333dc
Jay Wood <jwoodcodes@gmail.com>
2025-02-01 21:25:16 -0600

    articles in prisma postgres db now

---
+++ createArticle.js
@@ -0,0 +1,31 @@
+// createArticle.js
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const title = process.argv[2];
+  const author = process.argv[3];
+  const filePath = process.argv[4];
+  const imageUrl = process.argv[5] || null; // Optional
+
+  const newArticle = await prisma.article.create({
+    data: {
+      title,
+      author,
+      filePath,
+      imageUrl,
+    },
+  });
+
+  console.log('New Article Created:', newArticle);
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
