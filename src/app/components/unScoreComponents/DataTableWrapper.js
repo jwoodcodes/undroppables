2f248bc4b35b46b89cefb85d0005998e9d17d3e5
Jay Wood <jwoodcodes@gmail.com>
2025-05-12 20:26:52 -0500

    main page typscript conversion

--- src/app/components/unScoreComponents/DataTableWrapper.js
+++ src/app/components/unScoreComponents/DataTableWrapper.js
@@ -3,5 +3,6 @@
 import DataTable from './DataTable';
 
 export default function DataTableWrapper({ initialData }) {
+    console.log(initialData)
     return <DataTable data={initialData} />;
 }
--- src/app/unscore/page.tsx
+++ src/app/unscore/page.tsx
@@ -10,8 +10,45 @@ import { fetchData } from "../components/unScoreComponents/DataFetcher";
 import DataTableWrapper from "../components/unScoreComponents/DataTableWrapper";
 
 // Define the type for the data fetched
+
+
 type DataType = {
-  // Define the structure of your data here
+  careerAveragedStats: {
+    "1D/RR": number;
+    "1D/Snap": number;
+    "Adjusted Yds Per Team Play": number;
+    "Dominator Rating": number;
+    "EPA Per Play": number;
+    "EPA Per Team Pass Attempt": number;
+    "MTF/REC %": number;
+    "Man YPRR": number;
+    "PPR Points": number;
+    "REC Grade": number;
+    "RR": number;
+    "Rec": number;
+    "Rec TD MS": number;
+    "Rec TDs": number;
+    "Rec TDs per Game": number;
+    "Rec Yds": number;
+    "Rec Yds MS": number;
+    "Rec Yds Per Team Pass Att": number;
+    "Rec Yds per Game": number;
+    "Rec per Game": number;
+    "Reception MS (Games Played)": number;
+    "Rush Att per Game": number;
+    "Scrimmage Yds Per Team Pass Attempt": number;
+    "Scrimmage Yds Per Team Play": number;
+    "TD Per Team Pass Att": number;
+    "TGTs/G": number;
+    "TPRR": number;
+    "Targets": number;
+    "Total EPA": number;
+    "Weighted Dominator Rating": number;
+    "YAC/Rec": number;
+    "YPRR": number;
+    "Yards Per Rec": number;
+    "Zone YPRR": number;
+  }
 };
 
 export default async function unscore(): Promise<JSX.Element> {
