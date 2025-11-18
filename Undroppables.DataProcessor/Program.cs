7793f0079a7f23c56c9b1406774103aa5fca6f62
Jay Wood <jwoodcodes@gmail.com>
2025-11-17 20:43:48 -0600

    started C# migration

---
+++ Undroppables.DataProcessor/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+
+// See https://aka.ms/new-console-template for more information
+var builder = Host.CreateApplicationBuilder(args);
+
+var host = builder.Build();
+
+Console.WriteLine("Hello, World! The Data Processor is running.");
+
+host.Run();
